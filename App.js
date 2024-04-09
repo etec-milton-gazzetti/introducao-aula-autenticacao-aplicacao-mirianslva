f9b065625031d464d1aeecce66346823c4cace9a
mirianslva <142011303+mirianslva@users.noreply.github.com>
2024-04-09 15:39:54 +0000

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import SignIn from './src/screens/SignIn';
-import { View } from 'react-native';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import {ImageBackground } from '@gluestack-ui/themed';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Button,  Image, Text } from "@gluestack-ui/themed";
+import { useState } from "react";
+import fundo from "../../../assets/fundo.jpg"
 
-export default function App() {
+
+function HomeScreen({ navigation }) {
   return (
-    <View>
-    <SignIn />
-      <StatusBar style="auto" />
-    </ View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Tela Inicial</Text>
+      <Button title='Sobre' onPress={()=> navigation.navigate("Sobre")}/> 
+          </View>
   );
 }
-
+function Sobre({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Sobre</Text>
+      <Button title='Voltar' onPress={()=> navigation.goBack()}/> 
+      <ImageBackground>
+      </ImageBackground>
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+       <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Sobre" component={Sobre} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
