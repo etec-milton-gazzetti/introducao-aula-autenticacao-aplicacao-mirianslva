fd7f7606ea6ea605ea64b77b0c26b4623a409b6b
mirianslva <142011303+mirianslva@users.noreply.github.com>
2024-04-10 14:10:23 +0000

    first commit

--- App.js
+++ App.js
@@ -1,27 +1,29 @@
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button,Image } from 'react-native';
 import { NavigationContainer} from '@react-navigation/native';
 import {ImageBackground } from '@gluestack-ui/themed';
+import { useFonts } from 'expo-font';
 import { createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Button,  Image, Text } from "@gluestack-ui/themed";
-import { useState } from "react";
-import fundo from "../../../assets/fundo.jpg"
+import fundo from "./assets/fundo.jpg";
+
 
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <ImageBackground source={fundo} resizeMode="cover"/>
       <Text>Tela Inicial</Text>
       <Button title='Sobre' onPress={()=> navigation.navigate("Sobre")}/> 
           </View>
   );
 }
-function Sobre({ navigation }) {
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-SemiBoldItalic': 'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
+  });
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Sobre</Text>
-      <Button title='Voltar' onPress={()=> navigation.goBack()}/> 
-      <ImageBackground>
-      </ImageBackground>
+    <View style={styles.container}>
+      <Text style={{ fontFamily: 'Inter-SemiBoldItalic', fontSize: 30 }}>Inter SemiBoldItalic</Text>
+      <Text style={{ fontSize: 30 }}>Platform Default</Text>
     </View>
   );
 }
@@ -31,6 +33,7 @@ export default function App() {
     <NavigationContainer>
        <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Historia" component={Historia} />
         <Stack.Screen name="Sobre" component={Sobre} />
       </Stack.Navigator>
     </NavigationContainer>
